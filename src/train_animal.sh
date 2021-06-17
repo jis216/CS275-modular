eb88dde5fc5576f9c4a16f5c2f0bbdc7ae164a0f
jis216 <jiayuesun36@gmail.com>
2021-06-17 08:06:14 +0000

    update commands

--- src/train_animal.sh
+++
@@ -1,1 +0,0 @@
-python main.py --expID 004 --morphologies hopper walker_7 ostrich kangaroo
---
+++ src/train_animal_flip_att.sh
@@ -0,0 +1,1 @@
+python main.py --expID 006 --use_att --morphologies hopper_3 hopper_4 walker_7 ostrich kangaroo
