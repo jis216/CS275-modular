4fd30d6e2f6951f6cd5dd9db933797e27c62bd7c
jis216 <jiayuesun36@gmail.com>
2021-06-18 17:39:11 -0700

    commands

--- src/train_animal_main_att.sh
+++ src/train_animal_main_att.sh
@@ -1,1 +1,1 @@
-python main.py --expID 004 --use_att --morphologies hopper_4 hopper_3 walker_5_main walker_7_main ostrich_main kangaroo_main
+python main.py --expID 004 --use_att --morphologies hopper_4 hopper_3_main walker_5_main walker_7_main ostrich_main kangaroo_main
