9c62011f2fadb15dc46d5cb1fcf81e61f0751ffa
jis216 <jiayuesun36@gmail.com>
2021-06-18 22:11:32 -0700

    another set of hoppers

--- src/visualize_ant_hopper.sh
+++ src/visualize_ant_hopper.sh
@@ -1,2 +1,1 @@
-python visualize.py --expID 0009 --morphologies ant_three_legs
 python visualize.py --expID 0010 --morphologies hopper_three
---
+++ src/visualize_hopper.sh
@@ -0,0 +1,1 @@
+python visualize.py --expID 0010 --morphologies hopper_3
