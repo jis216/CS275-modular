9a0eb34b67472800892836acd6f41913af3b8f3e
jis216 <jiayuesun36@gmail.com>
2021-06-19 12:34:07 -0700

    update videos

---
+++ src/visualize_walker.sh
@@ -0,0 +1,1 @@
+python visualzie.py --expID 001 --morphologies walker_2 walker_3 walker_4 walker_5 walker_6 walker_7
---
+++ src/visualize_walker_att.sh
@@ -0,0 +1,1 @@
+python visualize.py --expID 002 --use_att --morphologies walker_2 walker_3 walker_4 walker_5 walker_6 walker_7
